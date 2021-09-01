f0be3fb3d01ea8475e1077dfbad9a21415a4b01b
ToniJakovcevic10 <toni.jakovcevic10@gmail.com>
2021-09-01 13:35:22 +0200

    mirrors and laser bouncing

--- Laser_Beam/LaserBeam.gd
+++ Laser_Beam/LaserBeam.gd
@@ -6,6 +6,60 @@ const MAX_LENGTH=2000
 onready var beam = $Beam
 onready var end = $End
 onready var raycast2D = $RayCast2D
+onready var line = $Line2D
+onready var line_ray=$Line_Ray
+
+
+
+func _process(_delta):
+	line.clear_points()
+	
+	if Input.is_mouse_button_pressed(BUTTON_RIGHT):
+		line.add_point(Vector2.ZERO)
+		
+		line_ray.global_position = line.global_position
+		line_ray.cast_to = (get_global_mouse_position() - line.global_position).normalized()*2000
+		line_ray.force_raycast_update()
+		
+		var previous = null
+		
+		while true:
+			if not line_ray.is_colliding():
+				var pt = line_ray.global_position + line_ray.cast_to
+				line.add_point(line.to_local(pt))
+				break
+			
+			var coll = line_ray.get_collider()
+			var pt = line_ray.get_collision_point()
+			
+			line.add_point(line.to_local(pt))
+			
+#			if not coll.collision_mask == 15:
+#				break
+			
+			var normal = line_ray.get_collision_normal()  #to make sure on which angle we are colliding
+			
+			if normal == Vector2.ZERO:
+				break                        #if collider starts out inside of hitbox or something similar
+			
+			#update collisions
+			if previous !=null:
+				previous.collision_mask = 3
+				previous.collision_layer = 3
+			previous = coll
+			previous.collision_mask = 0
+			previous.collision_layer = 0
+			
+			#update raycast
+			line_ray.global_position = pt
+			line_ray.cast_to = line_ray.cast_to.bounce(normal)
+			line_ray.force_raycast_update()
+
+		if previous !=null:
+				previous.collision_mask = 3
+				previous.collision_layer = 3
+
+
 
 func _physics_process(_delta):
 	var mouse_position = get_local_mouse_position()
@@ -17,4 +71,3 @@ func _physics_process(_delta):
 		end.global_position = raycast2D.cast_to
 	beam.rotation = raycast2D.cast_to.angle()
 	beam.region_rect.end.x = end.position.length()
-	
--- Singleton_script.gd
+++ Singleton_script.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-onready var opticalmedia = 1
-var air = preload("res://Optical_Media/Air/Air.tscn")
+onready var opticalmedia = 1  #zbog ovoga se odma pokrene uvik air
+#var air = preload("res://Optical_Media/Air/airNode.tscn")
 
 #choosing random number
 func choosing_random_number():
