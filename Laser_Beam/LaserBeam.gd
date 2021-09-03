e838e0c52a31be176d443dcd462e83b5a9ee8b5c
ToniJakovcevic10 <toni.jakovcevic10@gmail.com>
2021-09-03 13:20:04 +0200

    corrected bugs with diferent layers collisions

--- Laser_Beam/LaserBeam.gd
+++ Laser_Beam/LaserBeam.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var MAX_BOUNCES = 5
 const MAX_LENGTH=2000
-
+var LEVEL = 0
 
 
 
@@ -43,28 +43,7 @@ func _process(_delta):
 			line.add_point(line.to_local(pt))
 			
 					
-			if not coll.is_in_group("Mirrors"): #if it didnt hit mirror check for prism
-				if not coll.is_in_group("Prism"): #if it didnt hit prism break
-					break
-#				var normal = line_ray.get_collision_normal() #if it did hit prism make refraction
-#				if normal == Vector2.ZERO:
-#					break
-#				#update collisions
-#				if previous !=null:
-#					previous.collision_mask = 6
-#					previous.collision_layer = 6
-#				previous = coll
-#				#update raycast
-#				line_ray.global_position = pt
-#				line_ray.rotation += Vector2(1,4).angle() #have to change refraction angle with another func
-#				line_ray.cast_to = line_ray.cast_to.bounce(normal)
-#				line_ray.force_raycast_update()
-#				bounces += 1
-#				if bounces >= MAX_BOUNCES:
-#					break
-				check = false
-
-			if check ==true: #if it hit prism dont have to do all this again
+			if coll.is_in_group("Mirrors"): #if it didnt hit mirror check for prism
 				var normal = line_ray.get_collision_normal()  #to make sure on which angle we are colliding
 				if normal == Vector2.ZERO:
 					break                        #if collider starts out inside of hitbox or something similar
@@ -75,20 +54,52 @@ func _process(_delta):
 				previous = coll
 				previous.collision_mask = 0
 				previous.collision_layer = 0
-				#update raycast
 				line_ray.global_position = pt
 				line_ray.cast_to = line_ray.cast_to.bounce(normal)
 				line_ray.force_raycast_update()
-				bounces += 1
-				if bounces >= MAX_BOUNCES:
-					break
+			elif coll.is_in_group("Prism"):
+				var normal = line_ray.get_collision_normal()  #to make sure on which angle we are colliding
+				if normal == Vector2.ZERO:
+					break                        #if collider starts out inside of hitbox or something similar
+				#update collisions
+				if previous !=null:
+					previous.collision_mask = 3
+					previous.collision_layer = 3
+				previous = coll
+				previous.collision_mask = 0
+				previous.collision_layer = 0
+				line_ray.global_position = pt
+				line_ray.cast_to = line_ray.cast_to.bounce(Vector2(1,5).normalized())
+				line_ray.force_raycast_update()
+			else: 
+				break
+			bounces += 1
+			if bounces >= MAX_BOUNCES:
+				break
 
 		if previous !=null:
 			previous.collision_mask = 3
 			previous.collision_layer = 3
 		line_ray.rotation = 0
 
-
+#func prism_checker():
+#	#if not coll.is_in_group("Prism"): #if it didnt hit prism break
+#	var normal = line_ray.get_collision_normal() #if it did hit prism make refraction
+##	if normal == Vector2.ZERO:
+##		break
+#	#update collisions
+#	if previous !=null:
+#		previous.collision_mask = 6
+#		previous.collision_layer = 6
+#	previous = coll
+#	#update raycast
+#	line_ray.global_position = pt
+#	line_ray.rotation += Vector2(1,4).angle() #have to change refraction angle with another func
+#	line_ray.cast_to = line_ray.cast_to.bounce(normal)
+#	line_ray.force_raycast_update()
+#	bounces += 1
+##	if bounces >= MAX_BOUNCES:
+##		break
 
 
 #func _physics_process(_delta):
@@ -111,13 +122,20 @@ func calculate_angle(angle, size,n):
 	var fy = size * sin(angle)
 	
 
-
-func _on_prism_body_entered(body):
-	if body.name == "line_ray":
+func _physics_process(_delta):
+	if line_ray.is_colliding():
+		var coll = line_ray.get_collider()
 		var pt = line_ray.get_collision_point()
-		line.add_point(line.to_local(pt))
-		line_ray.global_position = pt
-		line_ray.cast_to = (get_global_mouse_position() - line.global_position).normalized()*2000
-		line_ray.rotation_degrees(20)
-		line_ray.force_raycast_update()
-		
+		if coll.is_in_group("Target"):
+			LEVEL += 1
+			print("congrats, you passed the level")
+
+#func _on_prism_body_entered(body):
+#	if body.name == "line_ray":
+#		var pt = line_ray.get_collision_point()
+#		line.add_point(line.to_local(pt))
+#		line_ray.global_position = pt
+#		line_ray.cast_to = (get_global_mouse_position() - line.global_position).normalized()*2000
+#		line_ray.rotation_degrees(20)
+#		line_ray.force_raycast_update()
+#
