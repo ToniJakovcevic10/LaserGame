080df4cac268b40db1c2251982e6b9de0ed93cb1
ToniJakovcevic10 <toni.jakovcevic10@gmail.com>
2021-09-21 20:43:43 +0200

    mirror instanced

--- World.gd
+++ World.gd
@@ -2,37 +2,37 @@ extends Node2D
 
 var obstac_number: int = 1
 
+onready var mirr = preload("res://Mirror/Mirror.tscn")
 onready var target: Node2D = $NodeTarget
 onready var mirror: StaticBody2D = $Mirror
 onready var mirror2: StaticBody2D = $Mirror2
-onready var mirror4: StaticBody2D = $Mirror4
-onready var mirror5: StaticBody2D = $Mirror5
 #onready var world = load("res://World.tscn")
 
 func _ready() ->void:
 	PlayerData.connect("score_updated", self, "_update_mirror")
 	PlayerData.connect("score_updated", self, "_update_mirror2")
 	PlayerData.connect("score_updated", self, "_update_target")
-	obstac_number = PlayerData.score
+	obstac_number = int(sqrt(PlayerData.score))
 	_update_mirror()
 	_update_target()
 	_update_mirror2()
-	if PlayerData.score >= 5:
-		mirror4.visible = true
-		mirror4.collision_layer = 3
-	if PlayerData.score >= 10:
-		mirror5.visible = true
-		mirror5.collision_layer = 3
-
+	while obstac_number>=2:
+		var mirror_instance = mirr.instance()
+		mirror_instance.position = Vector2(mirror_random_generator_number_x(),mirror_random_generator_number_y())
+		mirror_instance.rotation_degrees=randi()%180
+		add_child(mirror_instance)
+		mirror_instance.add_to_group("Mirrors")
+		obstac_number-=1
 
 func _update_mirror():
 	randomize()
-	mirror.position = Vector2(mirror_random_generator_number(), obstacle_random_generator_number())
-	#mirror.rotation_degrees = - randi()%45 + 90
+	mirror.position = Vector2(mirror_random_generator_number_x(), mirror_random_generator_number_y())
+	if mirror.position.y >= 640:
+		mirror.rotation_degrees = 90
 
 func _update_mirror2():
 	randomize()
-	mirror2.position = Vector2(620, obstacle_random_generator_number())
+	mirror2.position = Vector2(1,2)
 
 #func mirror_creation():
 #	var mirror_instance = mirror2.instance()
@@ -44,16 +44,12 @@ func _update_target():
 	randomize()
 	target.position = Vector2(randi()%180 + 770, randi()%500 + 50)
 
-func mirror_random_generator_number():
+func mirror_random_generator_number_x():
 	randomize()
-	var random =randi()%450
-	if random >=280 and random<=450:
-		random += 170
+	var random =randi()%530 + 180
 	return random
 
-func obstacle_random_generator_number():
+func mirror_random_generator_number_y():
 	randomize()
-	var random = randi()%500 + 30
-	if random >= 270 and random <= 330 :
-		random += 60
+	var random = randi()%400 + 60
 	return random 
